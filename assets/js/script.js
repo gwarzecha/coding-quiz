e1919a24985a31876341952a0ca39dd12b76cdb3
gwarz <gmwarzecha@tutanota.com>
2021-01-07 15:02:55 -0700

    intital folder setup

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+// create a timer that begins once the start button is clicked (addEventListener)
+// present a question with multiple clickable choices
+// present another question if the question is answered correctly
+// if a question is answered incorrectly, take time from the clock
+// timer reaches 0 when all questions are answered
+// when game is over, save initials and score (localStorage)
+
+var timer = function() {
+  var remainingTime = 60;
+
+  
+}
